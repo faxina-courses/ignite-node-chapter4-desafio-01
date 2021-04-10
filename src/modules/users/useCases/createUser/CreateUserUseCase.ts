7cae665881b8b95d55c4d757012966bb55ff7ff6
Tiago Faxina <thfxina@gmail.com>
2021-04-10 16:38:50 -0300

    Add CreateUserUseCase and ShowUserProfileuseCase tests

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,5 @@
 import { inject, injectable } from "tsyringe";
-import { hash } from 'bcryptjs';
+import { hash } from "bcryptjs";
 
 import { CreateUserError } from "./CreateUserError";
 
@@ -9,13 +9,12 @@ import { ICreateUserDTO } from "./ICreateUserDTO";
 @injectable()
 export class CreateUserUseCase {
   constructor(
-    @inject('UsersRepository')
-    private usersRepository: IUsersRepository,
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
   ) {}
 
   async execute({ name, email, password }: ICreateUserDTO) {
     const userAlreadyExists = await this.usersRepository.findByEmail(email);
-
     if (userAlreadyExists) {
       throw new CreateUserError();
     }
